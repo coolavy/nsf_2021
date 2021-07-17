6c5f0b24f4d3fc92235fa187bd293aa57595f9f9
coolavy <avyangupta@gmail.com>
2021-07-16 17:41:15 -0700

    problem5

---
+++ Homework_Week_2/problem5.py
@@ -0,0 +1,7 @@
+a = int(input("Please input first base of the trapeziod: "))
+b = int(input("Please input second base of the trapeziod: "))
+h = int(input("Please input the height of the trapezoid: "))
+
+area = ((a + b) / 2) * h
+
+print(f"The area of the trapeziod is {area}")
