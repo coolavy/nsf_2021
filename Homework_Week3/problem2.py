237ffd0de93983cdbf7d1f8976d83f560a32bbc2
coolavy <avyangupta@gmail.com>
2021-07-23 10:01:29 -0700

    problem 2

---
+++ Homework_Week3/problem2.py
@@ -0,0 +1,14 @@
+month = int(input("Enter the month as a number: "))
+day = int(input("Enter the date: "))
+
+if (month == 3 and day >= 20) or month in (4, 5) or (month == 6 and day < 20):
+    print("It is spring.")
+
+if (month == 6 and day >= 20) or month in (7, 8) or (month == 9 and day < 22):
+    print("It is summer")
+
+if (month == 9 and day >= 22) or month in (10, 11) or (month == 12 and day < 21):
+    print("It is fall.")
+
+if (month == 12 and day >= 21) or month in (1, 2) or (month == 3 and day < 20):
+    print("It is winter.")
