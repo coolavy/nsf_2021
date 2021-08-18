36a0a75c6264889d2ae06d34af6727bc87880a55
coolavy <avyangupta@gmail.com>
2021-08-18 10:49:42 -0700

    problem 1

--- Homework_Week_6/problem1.py
+++ Homework_Week_6/problem1.py
@@ -1,3 +1,4 @@
+import time
 def fib(x):
     sequence = [0,1]
     for n in range(x):
@@ -5,6 +6,11 @@ def fib(x):
     # print(sequence)
     return sequence[x]
 x = int(input("Enter n: "))
+t0 = time.time()
 
 result = fib(x)
+t1 = time.time()
 print(f"fib({x}) is {result}")
+result = str(result)
+print(f"{result} is {len(result)} digits long.")
+print(f"It took {t1 - t0:.15f}")
